f89e1e9422e2fd405e866e87f651e2e35058b0a9
Duke <dukemorise33@gmail.com>
2023-09-07 20:57:11 -0700

    deleted login and create-account HTML via Demetrio's guidance and added app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+import hideEveryThingButMainTest from "./JS_functions/testerFunctions.js";
+
+document.addEventListener('DOMContentLoaded',function(e){
+    e.preventDefault();
+    hideEveryThingButMainTest();
+})
+
+console.log('really?');
