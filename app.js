ffb6e8f5f5de8551dafb80d9614bd35c2880fec0
Duke <dukemorise33@gmail.com>
2023-09-07 22:15:46 -0700

    finished styling search bar wrapper and children

--- app.js
+++
@@ -1,8 +0,0 @@
-import hideEveryThingButMainTest from "./JS_functions/testerFunctions.js";
-
-document.addEventListener('DOMContentLoaded',function(e){
-    e.preventDefault();
-    hideEveryThingButMainTest();
-})
-
-console.log('really?');
